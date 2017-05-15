3cc3efdb8fc3453363fa699f44b5967a024d7cf5
somnussyq <1476602465@qq.com>
2017-05-15 22:16:09 +0800

    repaire pca

--- method/PCASelectUsers.py
+++ method/PCASelectUsers.py
@@ -1,5 +1,6 @@
 from baseclass.SDetection import SDetection
 from sklearn.metrics import classification_report
+from sklearn import preprocessing
 import numpy as np
 #from sklearn import preprocessing
 import math
@@ -24,19 +25,37 @@ class PCASelectUsers(SDetection):
                 a = int(user) - 1
                 b = int(item) - 1
                 dataArray[a][b] = value
-        #T
+
+        dataArray = preprocessing.scale(dataArray, axis=0)
         dataArrayT = np.transpose(dataArray)
         #cov
         covArray = np.dot(dataArrayT, dataArray)
         #eigen-value-decomposition
         vals, vecs  = np.linalg.eig(covArray)
-        # #top-K vals of cov
-        k = 3
+        #top-K vals of cov
+        k = 5
         valsInd = np.argsort(vals)
-        valsInd = valsInd[-1:-(k + 1):-1]
-        vecsInd = vecs[:, valsInd]
+        #valsInd = valsInd[-1:-(k + 1):-1]
+        valsInd = valsInd[0:k:1]
+        #use np.real() to get real parts
+        vecsInd = np.real(vecs[:, valsInd])
+        print vecsInd
+        pca_1 = []
+        pca_2 = []
+        pca_3 = []
+        # pca_4 = []
+        # pca_5 = []
+        for i in vecsInd:
+            pca_1.append(i[0])
+            pca_2.append(i[1])
+            pca_3.append(i[2])
+            # pca_4.append(i[3])
+            # pca_5.append(i[4])
+        # print pca_1
+        # print pca_2
+        # print pca_3
 
-        newArray = np.dot(dataArray, np.real(vecsInd))
+        newArray = np.dot(dataArray**2, np.real(vecsInd))
         #print newArray
 
         distanceDict = {}
@@ -45,29 +64,28 @@ class PCASelectUsers(SDetection):
             distance = 0
             #print user
             for tmp in user:
-                distance += tmp**2
+                distance += tmp
             distanceDict[userId] = float(distance)
             userId += 1
-        print distanceDict
+        #print distanceDict
 
         disSort = sorted(distanceDict.iteritems(), key=lambda d: d[1], reverse=False)
-        print disSort
+        #print disSort
         spamList = []
 
-        i = 1
+        i = 0
         while i <= 0.1 * len(disSort):
             spam = disSort[i]
-            spamId = spam[0]
+            spamId = int(spam[0]-1)
             spamList.append(spamId)
             self.predLabels[spamId] = 1
             i += 1
-        print self.predLabels
 
         #trueLabels
         for user in self.dao.trainingSet_u:
             userInd = int(user) -1
             self.trueLabels[userInd] = self.labels[user]
-        print self.trueLabels
+        #print self.trueLabels
 
 
     def predict(self):
