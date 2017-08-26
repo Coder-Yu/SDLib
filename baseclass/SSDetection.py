2b2c10c6edd722db33a2eb991d51bc7574044a8c
coder-Yu <coderyy@outlook.com>
2017-08-26 18:39:45 +0800

    fix a bug

--- baseclass/SSDetection.py
+++ baseclass/SSDetection.py
@@ -8,5 +8,5 @@ from sklearn.metrics import classification_report
 class SSDetection(SDetection):
 
     def __init__(self,conf,trainingSet=None,testSet=None,labels=None,relation=list(),fold='[1]'):
-        super(SSDetection, self).__init__(conf,trainingSet,testSet,fold)
+        super(SSDetection, self).__init__(conf,trainingSet,testSet,labels,fold)
         self.sao = SocialDAO(self.config, relation)  # social relations access control
