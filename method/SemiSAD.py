9a1088820fbf613f018af0d4fcb1513cb9c8c14f
Coder-Yu <yuyu2223@163.com>
2017-09-12 21:53:41 +0800

    fix a bug in SemiSAD

--- method/SemiSAD.py
+++ method/SemiSAD.py
@@ -210,6 +210,8 @@ class SemiSAD(SDetection):
             #pred_labelsForTrainingUn = classifier.predict(X_test)
             print 'Enhanced classifier...'
             while 1:
+                if len(X_test)<=5: # min
+                    break         #min
                 proba_labelsForTrainingUn = classifier.predict_proba(X_test)
                 X_test_labels = np.hstack((X_test, proba_labelsForTrainingUn))
                 X_test_labels0_sort = sorted(X_test_labels,key=lambda x:x[5],reverse=True)
@@ -219,11 +221,15 @@ class SemiSAD(SDetection):
                     classifier.partial_fit(b, ['0','0','0','0','0'], classes=['0', '1'],sample_weight=np.ones(len(b), dtype=np.float) * self.Lambda)
                     X_test_labels = X_test_labels0_sort[5:]
                     X_test = a[5:]
-                X_test_labels0_sort = sorted(X_test_labels, key=lambda x: x[6], reverse=True)
-                if X_test_labels0_sort[4][5]<X_test_labels0_sort[4][6]:
+                if len(X_test)<6: # min
+                    break         #min
+
+                X_test_labels0_sort = sorted(X_test_labels, key=lambda x: x[5], reverse=True)
+                if X_test_labels0_sort[4][5]<=X_test_labels0_sort[4][6]: #min
                     a = map(lambda x: x[:5], X_test_labels0_sort)
                     b = a[0:5]
                     classifier.partial_fit(b, ['1', '1', '1', '1', '1'], classes=['0', '1'],sample_weight=np.ones(len(b), dtype=np.float) * 1)
+                    X_test_labels = X_test_labels0_sort[5:]  # min
                     X_test = a[5:]
                 if len(X_test)<6:
                     break
